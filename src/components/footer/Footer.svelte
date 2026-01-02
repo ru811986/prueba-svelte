32892950d68f89e0b9dbe837b5397eb5d393a5ed
ru811986 <ru811986@gmail.com>
2026-01-02 23:11:26 +0100

    Agregar el header y el footer

---
+++ src/components/footer/Footer.svelte
@@ -0,0 +1,9 @@
+<script>
+    const currentYear = new Date().getFullYear();
+</script>
+
+<footer>
+    <div class="blue text-white p-4 text-center">
+        <p>&copy; {currentYear} Mi Sitio Web. Todos los derechos reservados.</p>
+    </div>
+</footer>
---
+++ src/components/header/Header.svelte
@@ -0,0 +1,55 @@
+<script>
+  let showMenu = false;
+
+  function toggleNavbar() {
+    showMenu = !showMenu;
+  }
+</script>
+
+<div class="w-full blue shadow">
+  <nav
+    class="container px-6 py-8 mx-auto md:flex md:justify-between md:items-center"
+  >
+    <div class="flex items-center justify-between">
+      <a
+        class="text-xl font-bold text-white md:text-2xl hover:text-black"
+        href="/home"
+        >Mi Sitio Web
+      </a>
+      <!-- Mobile menu button -->
+      <button
+        type="button"
+        class="flex text-white hover:text-gray-300 focus:outline-none focus:text-gray-300 md:hidden"
+        on:click={toggleNavbar}
+        title="Menú"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </button>
+    </div>
+
+    <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+    <div
+      class="flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0 {showMenu
+        ? 'flex'
+        : 'hidden'}"
+    >
+      <a class="text-white hover:text-black" href="/home">Home</a>
+      <a class="text-white hover:text-black" href="/blog">Blogs</a>
+      <a class="text-white hover:text-black" href="/contact">Contact US</a>
+      <a class="text-white hover:text-black" href="/about">About Us</a>
+    </div>
+  </nav>
+</div>
